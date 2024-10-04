46aef356f7aa985094b6a543a310f92ed9c4c3cf
Atikmamu <robinatik50@gmail.com>
2024-10-04 17:21:11 +0600

    first commit

---
+++ summary.js
@@ -0,0 +1,16 @@
+/**
+ * 1. load and display data initialy
+ * 2. on click load and display data 
+ * 3. Complex object accessing 
+ * 4. toggle active class functionality 
+ * 5. search functionality 
+ * 6. load and display single data
+ * 7. display single category data 
+ * 8. load and display single category data 
+ * 9. Conditional Rendering if array don't have any data 
+ * 10. Conditional Rendering:: if a value empty
+ * 11. Conditional Rendering :: if a value length is zero 
+ * 
+ * 
+ * 
+ */
